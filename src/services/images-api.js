198543209fb3245b9bc80d31299db848b74fd01d
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-09-15 00:08:34 +0200

    api

--- src/services/images-api.js
+++ src/services/images-api.js
@@ -1,6 +1,6 @@
 export const fetchImages = (query = "", pageNumber = 1) => {
   return fetch(
-    `https://pixabay.com/api/?q=${query}&page=${pageNumber}&key=13128632-519e28f670cc6f8f58c4d9c9f&image_type=photo&orientation=horizontal&per_page=12`
+    `https://pixabay.com/api/?q=${query}&page=${pageNumber}&key=22516391-185885990a61958acb3a57b33&image_type=photo&orientation=horizontal&per_page=12`
   )
     .then((x) => new Promise((resolve) => setTimeout(() => resolve(x), 1000))) // Задержка добавлена для тестирования Loader
     .then((res) => res.json())
