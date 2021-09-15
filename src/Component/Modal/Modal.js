4140641bde72bd542fc9fc9aed2b11aea2a84b78
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-09-15 22:29:19 +0200

    final

--- src/Component/Modal/Modal.js
+++ src/Component/Modal/Modal.js
@@ -1,9 +1,9 @@
-import React, { Component, createRef } from 'react';
+import React, { Component, createRef } from "react";
 import { createPortal } from "react-dom";
-import PropTypes from 'prop-types';
-import styles from './Modal.module.css';
+import PropTypes from "prop-types";
+import styles from "./Modal.module.css";
 
-const MODAL_ROOT = document.querySelector('#modal-root');
+const MODAL_ROOT = document.querySelector("#modal-root");
 
 export default class Modal extends Component {
   static propTypes = {
@@ -14,24 +14,24 @@ export default class Modal extends Component {
   backdropRef = createRef();
 
   componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyPress);
+    window.addEventListener("keydown", this.handleKeyPress);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyPress);
+    window.removeEventListener("keydown", this.handleKeyPress);
   }
 
-  handleKeyPress = e => {
+  handleKeyPress = (e) => {
     console.log(e);
 
-    if (e.code !== 'Escape') {
+    if (e.code !== "Escape") {
       return;
     }
 
     this.props.onClose();
   };
 
-  handleBackdropClick = e => {
+  handleBackdropClick = (e) => {
     if (this.backdropRef.current && e.target !== this.backdropRef.current) {
       return;
     }
