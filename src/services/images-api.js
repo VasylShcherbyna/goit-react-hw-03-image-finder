d877b82f43fbb50cddb2f4689d19163d5b401880
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-09-19 22:29:04 +0200

    Error

--- src/services/images-api.js
+++ src/services/images-api.js
@@ -2,9 +2,14 @@ export const fetchImages = (query = "", pageNumber = 1) => {
   return fetch(
     `https://pixabay.com/api/?q=${query}&page=${pageNumber}&key=22516391-185885990a61958acb3a57b33&image_type=photo&orientation=horizontal&per_page=12`
   )
-    .then((x) => new Promise((resolve) => setTimeout(() => resolve(x), 1000))) // Задержка добавлена для тестирования Loader
     .then((res) => res.json())
-    .then((data) => data.hits);
+    .then((data) => {
+      if (data.hits.length === 0) {
+        throw new Error(`No photo on search query "${query}"`);
+      } else {
+        return data;
+      }
+    });
 };
 
 export { fetchImages as default };
