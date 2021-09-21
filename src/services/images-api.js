2f8fa6ff451d1c1aeb0a0ecc7828c56932eda1c3
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-09-22 01:38:38 +0200

    data.hits

--- src/services/images-api.js
+++ src/services/images-api.js
@@ -1,5 +1,4 @@
 export const fetchImages = (query = "", pageNumber = 1) => {
-
   return fetch(
     `https://pixabay.com/api/?q=${query}&page=${pageNumber}&key=22516391-185885990a61958acb3a57b33&image_type=photo&orientation=horizontal&per_page=12`
   )
@@ -8,7 +7,7 @@ export const fetchImages = (query = "", pageNumber = 1) => {
       if (data.hits.length === 0) {
         throw new Error(`No photo on search query "${query}"`);
       } else {
-        return data;
+        return data.hits;
       }
     });
 };
